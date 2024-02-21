9281cf45bbec077e5360f82e14e7a3f26350e92b
Jyothesh Karnam <96298283+jyotheshkar@users.noreply.github.com>
2024-02-21 13:45:33 +0000

    Update FinalAnalytics.py

--- FinalAnalytics.py
+++ FinalAnalytics.py
@@ -3,138 +3,96 @@ import matplotlib.pyplot as plt
 from scipy.stats import zscore
 import numpy as np
 import os
-import xlsxwriter  # Importing xlsxwriter for Excel file writing
+import xlsxwriter
 
 def plot_combined_booking_activity_analysis_and_display_table(file_path):
-    # To read the CSV file and to skip the first row if it is necessary
+    # Reading the CSV file, skipping the first row if necessary
     df = pd.read_csv(file_path, skiprows=1)
     
-    # To convert the 'Created Date' column to datetime format
+    # Converting 'Created Date' to datetime format
     df['Created Date'] = pd.to_datetime(df['Created Date'], format='%d/%m/%Y')
     
-    # To find the first and last dates in the data
+    # Finding the first and last dates
     start_date = df['Created Date'].min()
     end_date = df['Created Date'].max()
     
-    # To calculate the number of days and subtract to get to the nearest Monday 
+    # Calculating the nearest Monday
     days_to_monday = (start_date.weekday() - 0) % 7
-    
-    # Adjusting the start_date to the nearest Monday
     first_monday = start_date - pd.Timedelta(days=days_to_monday)
     
-    # To Calculate sequential week numbers which starts from the first monday
+    # Calculating sequential week numbers
     df['Sequential Week Number'] = ((df['Created Date'] - first_monday).dt.days // 7) + 1
-
-    # Creating a dataframe that wil list all sequential week numbers
     all_weeks = pd.DataFrame({'Sequential Week Number': range(1, ((end_date - first_monday).days // 7) + 2)})
     
-    # To group the data by week and then count the number of bookings per week 
+    # Grouping data by week
     booking_counts = df.groupby('Sequential Week Number').size().reset_index(name='Number of Bookings')
-    
-    # To merge the list of all the weeks with the booking counts and also to include the weeks with no booking counts
     booking_counts_complete = pd.merge(all_weeks, booking_counts, on='Sequential Week Number', how='left').fillna(0)
     
-    # Calculate the z-scores for the booking counts to identify the outliers
-    booking_counts_complete['z-score'] = zscore(booking_counts_complete['Number of Bookings'], ddof=0)
+    # Calculating z-scores for booking counts
+    booking_counts_complete['z-score'] = zscore(booking_counts_complete['Number of Bookings']).round(4)
     
-    # Round the z-scores to 4 decimal places 
-    booking_counts_complete['z-score'] = booking_counts_complete['z-score'].round(4)
-    
-    # start plotting
+    # Plotting
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(16, 12))
-    fig.patch.set_facecolor('white')  # changes the background to white
-    ax.set_facecolor('white')  # changes the plot background to white
-
-    # To plot the number of bookings per week in a bar chart 
+    fig.patch.set_facecolor('white')
+    ax.set_facecolor('white')
     ax.bar(booking_counts_complete['Sequential Week Number'], booking_counts_complete['Number of Bookings'], color='skyblue', alpha=0.7, width=1)
-
-    # for threshold value calculate the mean and standard deviation 
+    
+    # Calculating mean, std, and threshold
     mean_bookings = booking_counts_complete['Number of Bookings'].mean()
-    std_bookings = booking_counts_complete['Number of Bookings'].std(ddof=0)
+    std_bookings = booking_counts_complete['Number of Bookings'].std()
     threshold_value = mean_bookings + std_bookings
-
-    # To determine the offset for text annotations or tooltips based on the range of bookings each week 
     offset = max(booking_counts_complete['Number of Bookings']) * 0.02
-
-    # To annotate each bar with the total number of bookings and also highlighting the ones above the threshold in red
+    
+    # Annotating bars
     for i, row in booking_counts_complete.iterrows():
         color = 'red' if row['Number of Bookings'] > threshold_value else 'black'
-        ax.scatter(row['Sequential Week Number'], row['Number of Bookings'], color=color, zorder=5)
-        ax.text(row['Sequential Week Number'], row['Number of Bookings'] + offset, f"{int(row['Number of Bookings'])}", 
-                ha='center', va='center', color='white', fontsize=8, 
-                bbox=dict(facecolor=color, edgecolor='none', boxstyle="round,pad=0.3"))
+        ax.text(row['Sequential Week Number'], row['Number of Bookings'] + offset, f"{int(row['Number of Bookings'])}", ha='center', va='center', color=color, fontsize=8)
     
-    # Adjusts the x-axis tick labels based on the total number of weeks
-    if len(all_weeks) > 50:
-        tick_spacing = 5
-        ticks = range(0, len(all_weeks) + tick_spacing, tick_spacing)
-        ax.set_xticks(ticks)
-        ax.set_xticklabels(ticks, rotation=0)  # Display x-axis labels upright
-    else:
-        ax.set_xticks(range(1, booking_counts_complete['Sequential Week Number'].max() + 1))
-        ax.set_xticklabels(range(1, booking_counts_complete['Sequential Week Number'].max() + 1), rotation=0)
-
-    # Sets the axis title and labels
     ax.set_xlabel('Week No.')
     ax.set_ylabel('Number of Bookings')
     filename = os.path.basename(file_path)
     ax.set_title(f'Booking Count by Week ({filename})')
-
-    # Adjusts the grid, ticks and limits for better visualization
+    
     ax.grid(True, color='gray', linestyle='--', linewidth=0.5)
     ax.set_xlim(0.5, len(all_weeks) + 0.5)
     ax.set_ylim(0, max(booking_counts_complete['Number of Bookings']) + (5 * offset))
-
-    # To draw the threshold line in dotted format and annotate it with the threshold value
-    threshold_line = ax.axhline(y=threshold_value, color='darkorange', linestyle='--', linewidth=2)
-    ax.text(len(all_weeks) + 0.5, threshold_value, f"Threshold: {threshold_value:.2f}", 
-            va='center', ha='left', 
-            bbox=dict(facecolor='black', edgecolor='orange', boxstyle="round,pad=0.3", linewidth=2), color='orange')
-
-    plt.tight_layout()  # Layout Adjustments for clean visual
-    plt.show()  # Displays the plots
-
     
-    # This code saves the table data into an excel file
-    excel_filename = f"datareport_{os.path.splitext(os.path.basename(file_path))[0]}.xlsx"
-    excel_folder = os.path.join(os.path.dirname(file_path), "reports")
-    os.makedirs(excel_folder, exist_ok=True)  # This line automatically creates an reports file if there is none
+    ax.axhline(y=threshold_value, color='darkorange', linestyle='--', linewidth=2)
+    ax.text(len(all_weeks) + 0.5, threshold_value, f"Threshold: {threshold_value:.2f}", va='center', ha='left', color='orange')
+    
+    plt.tight_layout()
+    
+    # Dynamic image saving
+    base_directory = os.path.dirname(file_path)
+    filename_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+    image_directory = os.path.join(base_directory, "Images")
+    os.makedirs(image_directory, exist_ok=True)
+    image_file_path = os.path.join(image_directory, f"{filename_without_extension}.png")
+    plt.savefig(image_file_path, dpi=300, bbox_inches='tight')
+    print(f"Plot image saved at: {image_file_path}")
+    
+    plt.show()  # Displays the plot after saving
+    
+    # Excel report generation
+    excel_filename = f"datareport_{filename_without_extension}.xlsx"
+    excel_folder = os.path.join(base_directory, "reports")
+    os.makedirs(excel_folder, exist_ok=True)
     excel_filepath = os.path.join(excel_folder, excel_filename)
     workbook = xlsxwriter.Workbook(excel_filepath)
     worksheet = workbook.add_worksheet()
 
-    # Formatting the heading and styling 
     heading_format = workbook.add_format({'bold': True, 'font_color': 'white', 'bg_color': 'black', 'border': 1})
-    data_format = workbook.add_format({'border': 1, 'align': 'center'})
-    red_bg_format = workbook.add_format({'bg_color': 'red', 'border': 1, 'align': 'center'})
+    data_format = workbook.add_format({'border': 1, 'align': 'center', 'num_format': '0.0000'})
 
-    # This part writes the tables data for week numbers, number of bookings and z-score values preciesly
     headings = ['Sequential Week Number', 'Number of Bookings', 'z-score']
-    for col, heading in enumerate(headings):
-        worksheet.write(0, col, heading, heading_format)
-
-    for row, data in enumerate(booking_counts_complete.values):
-        for col, value in enumerate(data):
-            if headings[col] == 'z-score':
-                cell_format = red_bg_format if value > 1.000 else data_format
-            else:
-                cell_format = data_format
-            worksheet.write(row + 1, col, value, cell_format)
-
-    # this part is for the second table data that shows the threshold, mean and standard deviation values
-    worksheet.write(len(booking_counts_complete) + 2, 0, 'Threshold', heading_format)
-    worksheet.write(len(booking_counts_complete) + 2, 1, threshold_value, data_format)
-
-    worksheet.write(len(booking_counts_complete) + 3, 0, 'Mean', heading_format)
-    worksheet.write(len(booking_counts_complete) + 3, 1, mean_bookings, data_format)
-
-    worksheet.write(len(booking_counts_complete) + 4, 0, 'Standard Deviation', heading_format)
-    worksheet.write(len(booking_counts_complete) + 4, 1, std_bookings, data_format)
+    worksheet.write_row('A1', headings, heading_format)
+    for row, data in enumerate(booking_counts_complete.values, start=1):
+        worksheet.write_row(f'A{row+1}', data, data_format)
 
     workbook.close()
     print(f"Excel file saved successfully: {excel_filepath}")
 
-# In this file path variable. please change it into your file path where the csv files are located
-file_path = ''
+# Specify your CSV file path
+file_path = 'C:\\Users\\Jyothesh karnam\\Desktop\\collaborative application development\\Data Files\\SRM23.csv'
 plot_combined_booking_activity_analysis_and_display_table(file_path)
